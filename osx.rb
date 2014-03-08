c1acdc1b0bfc0ed808e529052d2a0081ada71a8a
Teddy <tku@thoughtworks.com>
2014-03-08 13:34:09 -0500

    add more osx apps

--- osx.rb
+++ osx.rb
@@ -2,22 +2,37 @@
 
 # osx apps
 
+dep "Dropbox.app" do
+  source "https://www.dropbox.com/download?plat=mac"
+end
+
 dep "Google Chrome.app" do
   source "https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg"
 end
 
-dep "Dropbox.app" do
-  source "https://www.dropbox.com/download?plat=mac"
+dep "KeePassX.app" do
+  source "https://www.keepassx.org/releases/KeePassX-0.4.3.dmg"
 end
 
 dep "iTerm.app" do
   source "http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"
 end
 
+dep "Skype.app" do
+  source "http://download.skype.com/macosx/Skype_6.14.59.351.dmg"
+end
+
+dep "Sublime Text.app" do
+  source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg"
+end
+
 dep "osx" do
-  requires "Google Chrome.app"
   requires "Dropbox.app"
+  requires "Google Chrome.app"
+  requires "KeePassX.app"
   requires "iTerm.app"
+  requires "Skype.app"
+  requires "Sublime Text.app"
   requires "auto-hide-dock"
 end
 
