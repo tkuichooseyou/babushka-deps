734b362c64feffa5fcb4fb72ae1186d998f6a715
Teddy <tku@thoughtworks.com>
2014-03-22 23:33:19 -0400

    added Flux

--- osx.rb
+++ osx.rb
@@ -14,6 +14,10 @@ dep "Firefox.app" do
   source "https://download-installer.cdn.mozilla.net/pub/firefox/releases/27.0.1/mac/en-US/Firefox%2027.0.1.dmg"
 end
 
+dep "Flux.app" do
+  source "https://justgetflux.com/mac/Flux.zip"
+end
+
 dep "Google Chrome.app" do
   source "https://dl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg"
 end
@@ -46,6 +50,7 @@ dep "osx" do
   requires "Android File Transfer.app"
   requires "Dropbox.app"
   requires "Firefox.app"
+  requires "Flux.app"
   requires "Google Chrome.app"
   requires "iTerm.app"
   requires "KeePassX.app"
