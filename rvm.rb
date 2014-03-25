f15884c263d34b235a02ce53e089879f65900f11
tkuichooseyou <teddypku@gmail.com>
2014-03-25 11:44:09 -0400

    fixed rvm to load right after installing

--- rvm.rb
+++ rvm.rb
@@ -5,6 +5,7 @@ dep 'rvm' do
 
   meet {
     shell '\curl -sSL https://get.rvm.io | bash -s stable --autolibs=homebrew --ruby'
+    shell 'bash -c "source ~/.rvm/scripts/rvm; rvm notes"'
     shell "echo \"
 # RVM
 [[ -s '/Users/`whoami`/.rvm/scripts/rvm' ]] && source '/Users/`whoami`/.rvm/scripts/rvm'\" >> /etc/bashrc
