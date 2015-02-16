f88766a24e0cc896a772c8818253bd7f1652b94f
Teddy Ku <teddypku@gmail.com>
2015-02-16 10:07:19 -0800

    Update osx.rb

--- osx.rb
+++ osx.rb
@@ -2,10 +2,6 @@
 
 # osx apps
 
-dep "Android File Transfer.app" do
-  source "https://dl.google.com/dl/androidjumper/mtp/502266/androidfiletransfer.dmg"
-end
-
 dep "Dropbox.app" do
   source "https://www.dropbox.com/download?plat=mac"
 end
@@ -26,20 +22,8 @@ dep "iTerm.app" do
   source "http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"
 end
 
-dep "KeePassX.app" do
-  source "https://www.keepassx.org/releases/KeePassX-0.4.3.dmg"
-end
-
-dep "PCKeyboardHack.app" do
-  source "https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.5.0.dmg"
-end
-
-dep "Skype.app" do
-  source "http://download.skype.com/macosx/Skype_6.14.59.351.dmg"
-end
-
-dep "Sublime Text.app" do
-  source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg"
+dep "Seil.app" do
+  source "https://pqrs.org/osx/karabiner/files/Seil-11.0.0.dmg"
 end
 
 dep "VLC.app" do
@@ -47,16 +31,12 @@ dep "VLC.app" do
 end
 
 dep "osx" do
-  requires "Android File Transfer.app"
   requires "Dropbox.app"
   requires "Firefox.app"
   requires "Flux.app"
   requires "Google Chrome.app"
   requires "iTerm.app"
-  requires "KeePassX.app"
-  requires "PCKeyboardHack.app"
-  requires "Skype.app"
-  requires "Sublime Text.app"
+  requires "Seil.app"
   requires "VLC.app"
   requires "auto-hide-dock"
 end
