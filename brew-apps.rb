d9a21247f288c364bcef1dfbd8644befd5383d15
Teddy Ku <teddypku@gmail.com>
2015-03-26 13:20:25 -0400

    reformatted brew-apps

--- brew-apps.rb
+++ brew-apps.rb
@@ -1,50 +1,27 @@
 # brew-apps.rb
 
-dep "ack.bin" do
-  installs "ack"
-end
-dep "autoconf.bin" do
-  installs "autoconf"
-end
-dep "automake.bin" do
-  installs "automake"
-end
-dep "carthage.bin" do
-  installs "carthage"
-end
-dep "ctags.bin" do
-  installs "ctags"
-end
-dep "git.bin" do
-  installs "git"
-end
+dep "ack.bin"
+dep "autoconf.bin"
+dep "automake.bin"
+dep "carthage.bin"
+dep "ctags.bin"
+dep "git.bin"
+
 dep "heroku.bin" do
   installs "heroku-toolbelt"
 end
+
 dep "macvim.bin" do
   provides "mvim"
 end
-dep "node.bin" do
-  installs "node"
-end
-dep "openssl.bin" do
-  installs "openssl"
-end
-dep "reattach-to-user-namespace.bin" do
-  installs "reattach-to-user-namespace.bin"
-end
-dep "tig.bin" do
-  installs "tig"
-end
-dep "vim.bin" do
-  installs "vim"
-end
-dep "xctester.bin" do
-  installs "xctester"
-end
-dep "zsh.bin" do
-  installs "zsh"
-end
+
+dep "node.bin"
+dep "openssl.bin"
+dep "reattach-to-user-namespace.bin"
+dep "tig.bin"
+dep "vim.bin"
+dep "xctester.bin"
+dep "zsh.bin"
 
 dep "brew-apps" do
   requires "ack.bin"
