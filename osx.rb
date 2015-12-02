e5562a7236e2646d6e0dd10d74914074708e5b13
Teddy Ku <teddypku@gmail.com>
2015-12-02 16:02:34 +0800

    updated - removed macvim, replaced seil with karabiner, added HWMonitor

--- osx.rb
+++ osx.rb
@@ -1,17 +1,22 @@
 # osx.rb
 
 # osx apps
+# http://d.goodlad.net/articles/day_to_day_babushka/
 
 dep "Flux.app" do
   source "https://justgetflux.com/mac/Flux.zip"
+end
+
+dep "HWMonitor.installer" do
+  source "http://hwsensors.com/content/01-releases/39-release-1394/HWSensors.6.18.1394.pkg"
 end
 
 dep "iTerm.app" do
   source "https://iterm2.com/downloads/beta/iTerm2-3_0_2.zip"
 end
 
-dep "Seil.app" do
-  source "https://pqrs.org/osx/karabiner/files/Seil-11.0.0.dmg"
+dep "Karabiner.app" do
+  source "https://pqrs.org/osx/karabiner/files/Karabiner-10.14.0.dmg"
 end
 
 dep "VLC.app" do
@@ -20,8 +25,9 @@ end
 
 dep "osx" do
   requires "Flux.app"
+  requires "HWMonitor.app"
   requires "iTerm.app"
-  requires "Seil.app"
+  requires "Karabiner.app"
   requires "VLC.app"
   requires "auto-hide-dock"
 end
