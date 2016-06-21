f4c98fe5d4400f3e416d4aee136f5b3ebc5f865b
Teddy Ku <tkuichooseyou@users.noreply.github.com>
2016-06-21 11:30:46 -0400

    Update brew-apps.rb

--- brew-apps.rb
+++ brew-apps.rb
@@ -3,19 +3,10 @@
 dep "ack.bin"
 dep "autoconf.bin"
 dep "automake.bin"
-dep "carthage.bin"
 dep "ctags.bin"
 dep "fasd.bin"
 dep "flow.bin"
 dep "git.bin"
-
-dep "heroku.bin" do
-  installs "heroku-toolbelt"
-end
-
-dep "macvim.bin" do
-  provides "mvim"
-end
 
 dep "node.bin"
 dep "openssl.bin"
@@ -23,26 +14,21 @@ dep "reattach-to-user-namespace.bin"
 dep "tig.bin"
 dep "vim.bin"
 dep "watchman.bin"
-dep "xctester.bin"
 dep "zsh.bin"
 
 dep "brew-apps" do
   requires "ack.bin"
   requires "autoconf.bin"
   requires "automake.bin"
-  requires "carthage.bin"
   requires "ctags.bin"
   requires "fasd.bin"
   requires "flow.bin"
   requires "git.bin"
-  requires "heroku.bin"
-  requires "macvim.bin"
   requires "node.bin"
   requires "openssl.bin"
   requires "reattach-to-user-namespace.bin"
   requires "tig.bin"
   requires "vim.bin"
   requires "watchman.bin"
-  requires "xctester.bin"
   requires "zsh.bin"
 end
