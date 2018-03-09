823cd9fd456c16d736a899eda44d1ed2533331e7
Teddy Ku <teddypku@gmail.com>
2018-03-09 14:59:16 +0900

    remove haskell and kawa, add cmd-eikana

--- casks.rb
+++ casks.rb
@@ -2,6 +2,10 @@ dep "charles", :template => "cask" do
   cask "charles"
 end
 
+dep "cmd-eikana", :template => "cask" do
+  cask "cmd-eikana"
+end
+
 dep "flux", :template => "cask" do
   cask "flux"
 end
@@ -9,10 +13,6 @@ end
 dep "iterm2", :template => "cask" do
   cask "iterm2"
   provides "iTerm"
-end
-
-dep "kawa", :template => "cask" do
-  cask "kawa"
 end
 
 dep "karabiner-elements", :template => "cask" do
@@ -33,9 +33,9 @@ end
 
 dep "casks" do
   requires "charles"
+  requires "cmd-eikana"
   requires "flux"
   requires "iterm2"
-  requires "kawa"
   requires "karabiner-elements"
   requires "keybase"
   requires "shiftit"
